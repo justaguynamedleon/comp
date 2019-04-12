843211e19ec5e8ecfba62582ca4365f46549bae3
leon <leonel.nascimento96@gmail.com>
2019-04-12 18:28:48 +0100

    First step

---
+++ comp/Compiler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp
+{
+    class Compiler
+    {
+        private Dictionary<string, string> t;
+
+        public Compiler(Dictionary<string, string> t)
+        {
+            this.t = t;
+        }
+
+        public string Print()
+        {
+            string s = "";
+            foreach (var item in t)
+            {
+                s += $"{item.Key} -> {item.Value}\n";
+            }
+            return s;
+        }
+
+        public string f(string x, string inp)
+        {
+            foreach (var s in x)
+            {
+                if (!terminal(s.ToString()))
+                {
+                    inp += f(t[s.ToString()], inp);
+                }
+                else
+                {
+                    //if(inp.Length > 0)
+                    //    return 
+                    if (s == inp[0])
+                        inp = inp.Substring(1);
+                    //else if (!terminal(s.ToString()))
+                    //    return f(s.ToString(), inp);
+                    //else
+                    //    return x;
+                }
+            }
+            return inp;
+        }
+
+        public bool terminal(string key) => !t.ContainsKey(key);
+    }
+}
---
+++ comp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace comp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var dict = new Dictionary<string, string>()
+            {
+                { "S", "abc" }
+            };
+            Compiler c = new Compiler(dict);
+            var input = "ab";
+            MessageBox.Show(c.f("S", input) == "" ? "YEAH" : "NO");
+            richTextBox1.Text = c.Print() + new string('-', 20) + "\nInput: " + input;
+        }
+    }
+}
